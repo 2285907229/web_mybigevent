1f57a00ffe24ccb9ef7265fac295d9f366d9a73e
babaofan <2285907229@qq.com>
2020-08-01 14:36:21 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,23 @@
 // 每次调用ajax请求时候会先调用ajaxPrefilter这个函数
 $.ajaxPrefilter(function (options) {
-    // console.log(options.url)
-    options.url = 'http://ajax.frontend.itheima.net' + options.url
-})
+  // console.log(options.url)
+  options.url = "http://ajax.frontend.itheima.net" + options.url;
+
+  // 统一为有权限的接口设置请求头
+  if (options.url.indexOf("/my/") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+
+  // 全局统一挂在complete函数
+  options.complete = function (res) {
+    // console.log('执行了complete函数')
+    // console.log(res)
+    // 在complete函数中可以使用res.responseJSON拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
+});
---
+++ assets/js/index.js
@@ -0,0 +1,63 @@
+$(function () {
+    getUserInfo()
+    
+    var layer = layui.layer
+    // 退出功能
+    $('#btnLogout').on('click', function () {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token')
+
+            location.href = '/login.html'
+
+            layer.close(index)
+        })
+        
+    })
+
+})
+
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        type:'get',
+        url:'/my/userinfo',
+        // headers: {
+        //     Authorization:localStorage.getItem('token') || ''
+        // },
+        success:function(res){
+            // console.log(res)
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        },
+        //无论成功还是失败。都会调用complete函数
+        // complete: function (res) {
+        //     console.log('执行了complete函数')
+        //     console.log(res)
+        //     // 在complete函数中可以使用res.responseJSON拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         localStorage.removeItem('token')
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 按需渲染头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 渲染文字头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
